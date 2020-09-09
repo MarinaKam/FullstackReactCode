bdad3502e99b8b64d3eb23182d184af020cea4bb
Marina <marina.stozhok@gmail.com>
2020-09-09 20:41:09 +0300

    feature: Added the ability login and logout from layout component, added redirect and dependencies from it.

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Box, Typography } from '@material-ui/core';
 
 export const Landing = () => {
   return (
-    <div>
-      Landing
-    </div>
+    <Box>
+      <Typography variant="h1" align="center">Emaily</Typography>
+      <Typography align="center">Collect feedback from your users</Typography>
+    </Box>
   );
 };
--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,26 +1,49 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link as RouterLink } from 'react-router-dom';
 import { mdiGoogle } from '@mdi/js';
 import { Icon } from '@mdi/react';
 import { Box, AppBar, Toolbar, Typography, Button, Link } from '@material-ui/core';
 import { SvgIcon } from '../SvgIcon';
 
 export const Layout = ({ children }) => {
+  const user = useSelector(({ auth }) => auth.user);
+
   return (
     <Box display="flex" flexDirection="column" height="100%" overflow="hidden">
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h4" component={Box} flexGrow={1}>
-            Emaily
-          </Typography>
+          <Box flexGrow={1}>
+            <Link
+              underline="none"
+              variant="h4"
+              color="inherit"
+              component={RouterLink}
+              to={!!user ? '/surveys' : '/'}
+            >
+              Emaily
+            </Link>
+          </Box>
 
-          <Button
-            color="inherit"
-            component={Link}
-            href="/auth/google"
-            startIcon={<SvgIcon><Icon path={mdiGoogle} /></SvgIcon>}
-          >
-            Login with Google
-          </Button>
+          {!user ?
+            <Button
+              color="inherit"
+              component={Link}
+              href="/auth/google"
+              startIcon={<SvgIcon><Icon path={mdiGoogle} /></SvgIcon>}
+            >
+              Login with Google
+            </Button>
+          :
+            <Button
+              color="inherit"
+              component={Link}
+              href="/api/logout"
+              startIcon={<SvgIcon><Icon path={mdiGoogle} /></SvgIcon>}
+            >
+              Log out
+            </Button>
+          }
         </Toolbar>
       </AppBar>
 
